9325a62bfb48c9a3694763e09276902a3588f037
Atila Avila <atilagarcia12@gmail.com>
2025-04-06 22:24:11 -0300

    substitui os campos pelos do exemplo dado

--- js/form.js
+++ js/form.js
@@ -1,20 +1,8 @@
-const headerLinks = document.querySelectorAll("header nav li");
 const campos = document.querySelectorAll(".fordform");
 const nome = document.getElementById("nomeid");
 const termos = document.getElementById("form__privacidade__termos");
 const botaoForm = document.getElementById("form__enviar");
 termos.checked = false;
-
-headerLinks.forEach((link) => {
-  link.addEventListener("mouseover", () => {
-    headerLinks[2].firstElementChild.classList.remove("headerLink");
-    link.firstElementChild.classList.add("headerLink");
-  });
-  link.addEventListener("mouseout", () => {
-    link.firstElementChild.classList.remove("headerLink");
-    headerLinks[2].firstElementChild.classList.add("headerLink");
-  });
-});
 
 termos.addEventListener("change", () => {
   if (termos.checked) {
@@ -27,12 +15,13 @@ termos.addEventListener("change", () => {
 });
 
 class contato {
-  constructor(nome, email, telefone, mensagem, tipoDeMensagem, promocoes) {
+  constructor(nome, email, cpf, sobrenome, telefone, tipoDeContato, promocoes) {
     this.nome = nome;
     this.email = email;
+    this.cpf = cpf;
+    this.sobrenome = sobrenome;
     this.telefone = telefone;
-    this.mensagem = mensagem;
-    this.tipoDeMensagem = tipoDeMensagem;
+    this.tipoDeContato = tipoDeContato;
     this.promocoes = promocoes;
   }
   static enviarInfo(respostaForm) {
@@ -44,8 +33,9 @@ function Post(form) {
   let data = new contato(
     form.elements.namedItem("nome").value,
     form.elements.namedItem("email").value,
+    form.elements.namedItem("cpf").value,
+    form.elements.namedItem("sobrenome").value,
     form.elements.namedItem("telefone").value,
-    form.elements.namedItem("mensagem").value,
     form.elements.namedItem("contato").value,
     form.elements.namedItem("form__privacidade__promocoes").checked
   );
